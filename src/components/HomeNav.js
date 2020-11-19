c6eb14c3b538727b4c7c026e767501c11d9498ed
snorliman <snorli@o2.pl>
2020-11-19 20:16:47 +0100

    fixed style in HomeHeader

--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -11,7 +11,7 @@ export default function HomeNav() {
             <li><Link to ="start">Start</Link></li>
             <li><Link to ="what-is-about">O co chodzi?</Link></li>
             <li><Link to ="about-us">O nas</Link></li>
-            <li><Link to ="start">Fundacje i organizacje</Link></li>
+            <li><Link to ="who-get-help">Fundacje i organizacje</Link></li>
             <li><Link to ="start">Kontakt</Link></li>
           </ul>
         </nav>
